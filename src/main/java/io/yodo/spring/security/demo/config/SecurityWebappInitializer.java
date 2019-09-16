f1303b82c3fe45b86af460a5933c3f2de5d88d7f
Joerg Henning <1712460+joerx@users.noreply.github.com>
2019-09-16 23:31:51 +0300

    Spring security setup

---
+++ src/main/java/io/yodo/spring/security/demo/config/SecurityWebappInitializer.java
@@ -0,0 +1,7 @@
+package io.yodo.spring.security.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebappInitializer extends AbstractSecurityWebApplicationInitializer {
+    // nothing to see here, move along
+}
